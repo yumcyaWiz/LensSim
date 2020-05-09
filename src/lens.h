2590d5b48b39470c589b8e2508af0b8819eca66a
yumcyawiz <hashdolf1923@gmail.com>
2020-05-10 03:52:13 +0900

    implement intersect with spherical lens

--- src/lens.h
+++ src/lens.h
@@ -1,7 +1,12 @@
 #ifndef _LENS_H
 #define _LENS_H
 
+#include <cmath>
+
+#include "hit.h"
+#include "ray.h"
 #include "vec3.h"
+
 using namespace Prl2;
 
 class Lens {
@@ -19,6 +24,22 @@ class Lens {
         thickness(_thickness),
         ior(_ior),
         z(0){};
+
+  bool intersect(const Ray& ray, Hit& res) const {
+    Vec3 center(0, 0, z + curvature_radius);
+    Real b = dot(ray.origin - center, ray.direction);
+    Real c = length2(ray.origin - center) - curvature_radius * curvature_radius;
+    Real D = b * b - c;
+    if (D < 0) return false;
+
+    Real t0 = -b - std::sqrt(D);
+    Real t1 = -b + std::sqrt(D);
+    Real t = curvature_radius * ray.direction.z() > 0 ? t0 : t1;
+
+    res.t = t;
+    res.hitPos = ray(t);
+    res.hitNormal = normalize(res.hitPos - center);
+  };
 };
 
 #endif
