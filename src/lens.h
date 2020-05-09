adc8bdd026ef4dfebc704a4e91fa86aa965c4df1
yumcyawiz <hashdolf1923@gmail.com>
2020-05-10 03:59:08 +0900

    implement intersect with aperture

--- src/lens.h
+++ src/lens.h
@@ -26,19 +26,36 @@ class Lens {
         z(0){};
 
   bool intersect(const Ray& ray, Hit& res) const {
-    Vec3 center(0, 0, z + curvature_radius);
-    Real b = dot(ray.origin - center, ray.direction);
-    Real c = length2(ray.origin - center) - curvature_radius * curvature_radius;
-    Real D = b * b - c;
-    if (D < 0) return false;
+    // aperture
+    if (curvature_radius == 0) {
+      Real t = -(ray.origin.z() - z) / ray.direction.z();
+      Vec3 hitPos = ray(t);
 
-    Real t0 = -b - std::sqrt(D);
-    Real t1 = -b + std::sqrt(D);
-    Real t = curvature_radius * ray.direction.z() > 0 ? t0 : t1;
+      Real r = hitPos.x() * hitPos.x() + hitPos.y() * hitPos.y();
+      if (r > aperture_radius * aperture_radius) return false;
 
-    res.t = t;
-    res.hitPos = ray(t);
-    res.hitNormal = normalize(res.hitPos - center);
+      res.t = t;
+      res.hitPos = ray(t);
+      res.hitNormal = Vec3(0, 0, -1);
+      return true;
+    }
+    // spherical lens
+    else {
+      Vec3 center(0, 0, z + curvature_radius);
+      Real b = dot(ray.origin - center, ray.direction);
+      Real c =
+          length2(ray.origin - center) - curvature_radius * curvature_radius;
+      Real D = b * b - c;
+      if (D < 0) return false;
+
+      Real t0 = -b - std::sqrt(D);
+      Real t1 = -b + std::sqrt(D);
+      Real t = curvature_radius * ray.direction.z() > 0 ? t0 : t1;
+
+      res.t = t;
+      res.hitPos = ray(t);
+      res.hitNormal = normalize(res.hitPos - center);
+    }
   };
 };
 
