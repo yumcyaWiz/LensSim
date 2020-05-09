ceaf9a19f14ff09bb699c4c31bfbdbc342585f14
yumcyawiz <hashdolf1923@gmail.com>
2020-05-10 04:37:41 +0900

    include reflect, refract from prl

--- src/lens-system.h
+++ src/lens-system.h
@@ -1,11 +1,37 @@
 #ifndef _LENS_SYSTEM_H
 #define _LENS_SYSTEM_H
 
+#include <cmath>
 #include <vector>
 
 #include "lens-element.h"
 
 using namespace Prl2;
+
+// 反射ベクトルを返す
+inline Vec3 reflect(const Vec3& v, const Vec3& n) {
+  return -v + 2 * dot(v, n) * n;
+}
+
+// フレネル係数を計算する
+inline Real fresnel(const Vec3& wo, const Vec3& n, const Real& n1,
+                    const Real& n2) {
+  const float f0 = std::pow((n1 - n2) / (n1 + n2), 2.0f);
+  return f0 + (1.0f - f0) * std::pow(1.0f - dot(wo, n), 5.0f);
+}
+
+// 屈折ベクトルを返す
+inline bool refract(const Vec3& wi, Vec3& wt, const Vec3& n, const Real& ior1,
+                    const Real& ior2) {
+  const Real eta = ior1 / ior2;
+  const Real cosThetaI = dot(wi, n);
+  const Real sin2ThetaI = std::max(0.0f, 1.0f - cosThetaI * cosThetaI);
+  const Real sin2ThetaT = eta * eta * sin2ThetaI;
+  if (sin2ThetaT >= 1.0f) return false;
+  const Real cosThetaT = std::sqrt(1.0f - sin2ThetaT);
+  wt = eta * (-wi) + (eta * cosThetaI - cosThetaT) * n;
+  return true;
+}
 
 class LensSystem {
  public:
