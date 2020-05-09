f567aac1d6698a5ddabfae8aa82d55593915e4df
yumcyawiz <hashdolf1923@gmail.com>
2020-05-10 03:26:21 +0900

    write lens constructor

--- src/lens.h
+++ src/lens.h
@@ -3,6 +3,20 @@
 
 class Lens {
  public:
+  unsigned int index;
+  double curvature_radius;
+  double aperture_radius;
+  double thickness;
+  double ior;
+  double z;
+
+  Lens(unsigned int _index, double _curvature_radius, double _thickness,
+       double _ior)
+      : index(_index),
+        aperture_radius(_curvature_radius),
+        thickness(_thickness),
+        ior(_ior),
+        z(0){};
 };
 
 #endif
