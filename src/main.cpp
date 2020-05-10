e1b48d11fd4fd37dcf0571b4982f6e7f4b26eba0
yumcyawiz <hashdolf1923@gmail.com>
2020-05-10 17:28:39 +0900

    test raytrace from object plane

--- src/main.cpp
+++ src/main.cpp
@@ -13,5 +13,11 @@ using json = nlohmann::json;
 int main() {
   LensSystem lsys("../data/dgauss.50mm.json");
 
+  Ray ray_in(Vec3(0, 0, 0), Vec3(0, 0, 1));
+  Ray ray_out;
+  if (lsys.raytraceFromObject(ray_in, ray_out)) {
+    std::cout << ray_out << std::endl;
+  }
+
   return 0;
 }
