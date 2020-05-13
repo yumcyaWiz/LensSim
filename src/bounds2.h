c345b0675f5ce73445ff6490625eb3c0a665f8f8
yumcyawiz <hashdolf1923@gmail.com>
2020-05-13 19:54:44 +0900

    impl extend bounds2

--- src/bounds2.h
+++ src/bounds2.h
@@ -13,4 +13,12 @@ class Bounds2 {
   Bounds2(const Vec2& _p0, const Vec2& _p1) : p0(_p0), p1(_p1) {}
 };
 
+inline Bounds2 extendBounds(const Bounds2& b, const Vec2& p) {
+  const Real p0x = std::min(b.p0.x(), p.x());
+  const Real p0y = std::min(b.p0.y(), p.y());
+  const Real p1x = std::max(b.p1.x(), p.x());
+  const Real p1y = std::max(b.p1.y(), p.y());
+  return Bounds2(Vec2(p0x, p0y), Vec2(p1x, p1y));
+}
+
 #endif
