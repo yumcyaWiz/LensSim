77decab5a50c086f55a3e7ed49e9bc0ef81f0884
yumcyawiz <hashdolf1923@gmail.com>
2020-05-15 03:45:24 +0900

    test sample ray

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,30 @@
 using json = nlohmann::json;
 
 // prl2
-#include "parallel.h"
+#include "random.h"
 
 // LensSym
 #include "lens-system.h"
 
 int main() {
+  std::shared_ptr<Sampler> sampler = std::make_shared<RandomSampler>();
+
   std::shared_ptr<Film> film = std::make_shared<Film>(512, 512);
   LensSystem lsys("../data/dgauss.50mm.json", film);
 
+  // Ray ray_in(Vec3(0, 0, 0), normalize(Vec3(0, 0, -1)));
+  // Ray ray_out;
+  // if (lsys.raytrace(ray_in, ray_out)) {
+  //   std::cout << ray_out << std::endl;
+  // }
+
   lsys.computeExitPupilBounds();
+  Ray ray_out;
+  for (int i = 0; i < 10; ++i) {
+    if (lsys.sampleRay(Vec2(0, 0), *sampler, ray_out)) {
+      std::cout << ray_out << std::endl;
+    }
+  }
 
   return 0;
 }
