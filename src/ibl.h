1ec27cf905456353914481e376a2e7b914b7b1de
yumcyawiz <hashdolf1923@gmail.com>
2020-05-15 04:51:54 +0900

    impl ibl

--- src/ibl.h
+++ src/ibl.h
@@ -3,18 +3,44 @@
 
 #include <string>
 
+// stb
+#include "stb_image.h"
+
+// prl2
+#include "core/ray.h"
 #include "core/vec3.h"
 
 using namespace Prl2;
 
 class IBL {
  public:
-  unsigned int width;
-  unsigned int height;
+  int width;
+  int height;
 
-  Vec3* pixels;
+  Real* pixels;
 
-  IBL(const std::string& filename) {}
+  IBL(const std::string& filename) {
+    int c;
+    pixels = stbi_loadf(filename.c_str(), &width, &height, &c, 3);
+  }
+  ~IBL() { stbi_image_free(pixels); }
+
+  Vec3 getRadiance(const Ray& ray) const {
+    Real theta, phi;
+    cartesianToSpherical(ray.direction, theta, phi);
+
+    const Real u = phi * INV_PI_MUL_2;
+    const Real v = theta * INV_PI;
+
+    const int i = u * width;
+    const int j = v * height;
+
+    const Real r = pixels[3 * i + 3 * width * j];
+    const Real g = pixels[3 * i + 3 * width * j + 1];
+    const Real b = pixels[3 * i + 3 * width * j + 2];
+
+    return Vec3(r, g, b);
+  }
 };
 
 #endif
