0940cc87605c4db5a826144345e2f3b120b9ed0e
yumcyawiz <hashdolf1923@gmail.com>
2020-05-15 05:18:19 +0900

    make ppm with sampled ray

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,53 @@
 #include <iostream>
+#include <string>
 
 // ext
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
 
-// prl2
+#include "ibl.h"
+#include "lens-system/lens-system.h"
+#include "parallel/parallel.h"
 #include "samplers/random.h"
 
-// LensSym
-#include "lens-system/lens-system.h"
-
 int main() {
+  constexpr int width = 128;
+  constexpr int height = 128;
+  constexpr int num_samples = 100;
+  const std::string path_to_lens = "../data/dgauss.50mm.json";
+
   std::shared_ptr<Sampler> sampler = std::make_shared<RandomSampler>();
 
-  std::shared_ptr<Film> film = std::make_shared<Film>(512, 512);
-  LensSystem lsys("../data/dgauss.50mm.json", film);
+  std::shared_ptr<Film> film = std::make_shared<Film>(width, height);
+  LensSystem lsys(path_to_lens, film);
 
   lsys.computeExitPupilBounds();
-  Ray ray_out;
-  for (int i = 0; i < 10; ++i) {
-    Real u = (i + 0.5) / 10.0f;
-    if (lsys.sampleRay(u, 0, *sampler, ray_out)) {
-      std::cout << ray_out << std::endl;
+
+  for (int j = 0; j < film->height; ++j) {
+    for (int i = 0; i < film->width; ++i) {
+      const Real u =
+          (2.0f * (i + sampler->getNext()) - film->width) / film->height;
+      const Real v =
+          (2.0f * (j + sampler->getNext()) - film->height) / film->height;
+
+      Vec3 col;
+      for (int k = 0; k < num_samples; ++k) {
+        // sample ray
+        Ray ray;
+        if (!lsys.sampleRay(u, v, *sampler, ray)) continue;
+        col += 0.5f * (ray.direction + 1.0f);
+      }
+
+      // divide by samples
+      col /= num_samples;
+
+      // set color on pixel
+      film->setPixel(i, j, col);
     }
   }
+
+  // output ppm
+  film->writePPM("output.ppm");
 
   return 0;
 }
