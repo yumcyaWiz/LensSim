50b16e37836b526bdb2ec067018eaab99daa8805
yumcyawiz <hashdolf1923@gmail.com>
2020-05-15 20:27:44 +0900

    impl exr output

--- src/film.h
+++ src/film.h
@@ -5,6 +5,10 @@
 #include <fstream>
 #include <iostream>
 
+// ext
+#include "tinyexr.h"
+
+// prl2
 #include "core/spectrum.h"
 #include "core/vec3.h"
 
@@ -90,6 +94,66 @@ class Film {
     }
 
     file.close();
+  }
+
+  void writeEXR(const std::string& filename) const {
+    EXRHeader header;
+    InitEXRHeader(&header);
+
+    EXRImage image;
+    InitEXRImage(&image);
+
+    image.num_channels = 3;
+
+    std::vector<float> images[3];
+    images[0].resize(width * height);
+    images[1].resize(width * height);
+    images[2].resize(width * height);
+
+    for (size_t i = 0; i < width * height; ++i) {
+      const Vec3 rgb = XYZ2RGB(pixels[i]);
+      images[0][i] = rgb.x();
+      images[1][i] = rgb.y();
+      images[2][i] = rgb.z();
+    }
+
+    float* image_ptr[3];
+    image_ptr[0] = &(images[2].at(0));
+    image_ptr[1] = &(images[1].at(0));
+    image_ptr[2] = &(images[0].at(0));
+
+    image.images = reinterpret_cast<unsigned char**>(image_ptr);
+    image.width = width;
+    image.height = height;
+
+    header.num_channels = 3;
+    header.channels = new EXRChannelInfo[header.num_channels];
+    strncpy(header.channels[0].name, "B", 255);
+    header.channels[0].name[strlen("B")] = '\0';
+    strncpy(header.channels[1].name, "G", 255);
+    header.channels[1].name[strlen("G")] = '\0';
+    strncpy(header.channels[2].name, "R", 255);
+    header.channels[2].name[strlen("R")] = '\0';
+
+    header.pixel_types = new int[header.num_channels];
+    header.requested_pixel_types = new int[header.num_channels];
+    for (int i = 0; i < header.num_channels; ++i) {
+      header.pixel_types[i] = TINYEXR_PIXELTYPE_FLOAT;
+      header.requested_pixel_types[i] = TINYEXR_PIXELTYPE_HALF;
+    }
+
+    const char* err = nullptr;
+    int ret = SaveEXRImageToFile(&image, &header, filename.c_str(), &err);
+    if (ret != TINYEXR_SUCCESS) {
+      fprintf(stderr, "Save EXR error: %s\n", err);
+      FreeEXRErrorMessage(err);
+      return;
+    }
+    printf("Saved EXR file. [%s] \n", filename.c_str());
+
+    delete[] header.channels;
+    delete[] header.pixel_types;
+    delete[] header.requested_pixel_types;
   }
 };
 
