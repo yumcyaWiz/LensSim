fefeca215792533564d0927b0dec7b5a2aed6b0d
yumcyawiz <hashdolf1923@gmail.com>
2020-09-16 18:44:29 +0900

    place CMakeLists.txt under test

--- test/test.cpp
+++ test/test.cpp
@@ -1,1 +1,9 @@
-int main() { return 0; }
+#include <memory>
+
+#include "lens-system/lens-system.h"
+
+int main() {
+  const auto film = std::make_shared<Film>(512, 512, 0.025, 0.025);
+  const LensSystem lsys = LensSystem("../data/dgauss100mm.json", film);
+  return 0;
+}
